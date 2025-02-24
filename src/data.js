39c12ea59897e8a4b9d7180356c8fef73fc4109d
Raúl Pineda <hola@raulpineda.mx>
2025-02-25 00:44:38 +0100

    Start building with Astro (#32)

---
+++ src/data.js
@@ -0,0 +1,99 @@
+import { ChartLineUp, Code, Globe, Robot, Wine } from "@phosphor-icons/react";
+
+// Site wide strings
+export const NAV_TEXT = "Raúl Pineda";
+export const SITE_TITLE = NAV_TEXT;
+export const SITE_DESCRIPTION = "A software development lead resume.";
+
+export const NAME = "Raúl";
+export const BIO =
+  "I am a software engineering leader based in Copenhagen, Denmark. I love working with teams who build products that have real impact on lots of people.";
+export const ABOUT_ME =
+  "I specialize in front-end development with a focus on creating user-friendly interfaces. My skills include HTML, CSS, JavaScript, and frameworks like React and Vue.js. I also have experience in back-end development using Node.js and Express, making me a versatile full-stack developer.";
+export const CONTACT_TEXT =
+  "I’d love to hear from you! Whether you have questions, collaboration ideas, or just want to connect, feel free to reach out. I’m always open to engaging conversations and exploring new opportunities.";
+export const NAV_LINKS = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/#about" },
+  { name: "Contact", href: "/#contact" },
+];
+export const TECHNICAL_SKILLS = [
+  "TypeScript",
+  "JavaScript",
+  "React",
+  "Nodejs",
+  "Python",
+  "Ruby on Rails",
+  "Kubernetes",
+  "Docker",
+];
+export const PERSONAL_SKILLS = [
+  "Engineering management",
+  "Career development",
+  "Public speaking",
+  "Project management",
+  "Training",
+  "Coaching",
+];
+export const LANGUAGES = [
+  "Spanish (Native)",
+  "English (Bilingual)",
+  "Danish (Conversational)",
+  "German (Basic)",
+];
+export const WORK_EXPERIENCES = [
+  {
+    organisationName: "DataRobot",
+    position: "Staff Engineer & Team Lead — Applications",
+    date: "2024 - PRESENT",
+    info: ["I worked on this", "I showed this skills"],
+    icon: Robot,
+  },
+  {
+    organisationName: "DataRobot",
+    position: "Staff Front End Engineer — MLOps",
+    date: "2022 - 2024",
+    info: ["I worked on this", "I showed this skills"],
+    icon: Robot,
+  },
+  {
+    organisationName: "DataRobot",
+    position: "Senior Front End Engineer — Inference",
+    date: "2019 - 2022",
+    info: ["I worked on this", "I showed this skills"],
+    icon: Robot,
+  },
+  {
+    organisationName: "Saxo Bank",
+    position: "Senior Front End Developer",
+    date: "2017 - 2019",
+    info: ["I worked on this", "I showed this skills"],
+    icon: ChartLineUp,
+  },
+  {
+    organisationName: "Vivino",
+    position: "Front End Developer",
+    date: "2015 - 2017",
+    info: ["I worked on this", "I showed this skills"],
+    icon: Wine,
+  },
+];
+export const VOLUNTEERING_EXPERIENCES = [
+  {
+    organisationName: "Codher",
+    date: "2016- 2019",
+    info: ["I worked on this", "I showed this skills"],
+    icon: Code,
+  },
+  {
+    organisationName: "AIESEC",
+    date: "2015 - 2017",
+    info: ["I worked on this", "I showed this skills"],
+    icon: Globe,
+  },
+];
+
+export const CONTACT_OPTIONS = [
+  { name: "Github", link: "https://github.com/raulpineda/" },
+  { name: "LinkedIn", link: "https://www.linkedin.com/in/raulpineda/" },
+];
