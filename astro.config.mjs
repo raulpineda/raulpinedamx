27ea0d529e4cc57a649fd57bec48d3d0e2f3f47a
Raúl Pineda <hola@raulpineda.mx>
2025-02-25 00:29:36 +0100

    Start building with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://raulpineda.mx",
+  integrations: [tailwind(), react(), sitemap()],
+});
